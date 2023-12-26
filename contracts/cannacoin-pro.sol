5b54a27301032932232c8478692f8c1b5aeae127
grasshaussoftware <deusopus@duck.com>
2023-12-25 18:38:06 -0800

    added yetiswap and traderjoe routers

--- contracts/cannacoin-pro.sol
+++ contracts/cannacoin-pro.sol
@@ -6,21 +6,31 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Cannacoin is ERC20, Ownable {
     uint256 public constant INITIAL_SUPPLY = 100000000 * (10 ** 18);
-    address public constant FEE_ADDRESS = 0x8114BeC86C8F56c1014f590E05cD7826054EcBdE;
+    address public constant FEE_ADDRESS =
+        0x8114BeC86C8F56c1014f590E05cD7826054EcBdE;
 
     // DEX Addresses
-    address public constant PANGOLIN_FACTORY = 0xefa94DE7a4656D787667C749f7E1223D71E9FD88;
-    address public constant PANGOLIN_ROUTER = 0xE54Ca86531e17Ef3616d22Ca28b0D458b6C89106;
-    address public constant TRADER_JOE_ROUTER = 0xYourTraderJoeRouterAddress; // Replace with Trader Joe Router address
-    address public constant YETISWAP_ROUTER = 0xYourYetiSwapRouterAddress; // Replace with YetiSwap Router address
+    address public constant PANGOLIN_FACTORY =
+        0xefa94DE7a4656D787667C749f7E1223D71E9FD88;
+    address public constant PANGOLIN_ROUTER =
+        0xE54Ca86531e17Ef3616d22Ca28b0D458b6C89106;
+    address public constant TRADER_JOE_ROUTER =
+        0x60aE616a2155Ee3d9A68541Ba4544862310933d4; // Replace with Trader Joe Router address
+    address public constant YETISWAP_ROUTER =
+        0x262DcFB36766C88E6A7a2953c16F8defc40c378A; // Replace with YetiSwap Router address
 
     event FeePaid(address indexed from, address indexed to, uint256 feeAmount);
 
-    constructor() ERC20("Cannacoin PRO", "CPRO") {
-        _mint(msg.sender, INITIAL_SUPPLY);
+    constructor(
+        address initialOwner
+    ) ERC20("Cannacoin PRO", "CPRO") Ownable(initialOwner) {
+        _mint(initialOwner, INITIAL_SUPPLY);
     }
 
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) public override returns (bool) {
         uint256 fee = 0;
         if (isDexTransaction(msg.sender, recipient)) {
             fee = calculateDexFee(amount); // 4.2% fee for DEX transactions
@@ -38,14 +48,22 @@ contract Cannacoin is ERC20, Ownable {
         return true;
     }
 
-    function isDexTransaction(address sender, address recipient) private view returns (bool) {
-        return sender == PANGOLIN_FACTORY || recipient == PANGOLIN_FACTORY ||
-               sender == PANGOLIN_ROUTER || recipient == PANGOLIN_ROUTER ||
-               sender == TRADER_JOE_ROUTER || recipient == TRADER_JOE_ROUTER ||
-               sender == YETISWAP_ROUTER || recipient == YETISWAP_ROUTER;
+    function isDexTransaction(
+        address sender,
+        address recipient
+    ) private pure returns (bool) {
+        return
+            sender == PANGOLIN_FACTORY ||
+            recipient == PANGOLIN_FACTORY ||
+            sender == PANGOLIN_ROUTER ||
+            recipient == PANGOLIN_ROUTER ||
+            sender == TRADER_JOE_ROUTER ||
+            recipient == TRADER_JOE_ROUTER ||
+            sender == YETISWAP_ROUTER ||
+            recipient == YETISWAP_ROUTER;
     }
 
     function calculateDexFee(uint256 _value) private pure returns (uint256) {
-        return _value * 42 / 1000; // 4.2% fee
+        return (_value * 42) / 1000; // 4.2% fee
     }
 }
