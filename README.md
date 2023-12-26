805d7286190178dc63b261cb6a683691a4252c71
grasshaussoftware <deusopus@duck.com>
2023-12-25 22:39:48 -0800

    code overhaul. two contracts now

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # cannacoin-pro
-stealth launch
-.42% transaction fee
-total supply 100M
-owner 0x8114BeC86C8F56c1014f590E05cD7826054EcBdE
+# stealth launch
+# 0.00042% transaction fee
+# 4.2% swap fee
+# total supply 100M
+# owner 0x8114BeC86C8F56c1014f590E05cD7826054EcBdE
+# team 0x3536b0152c91E60535508690a650C10bf09fe857
--- contracts/CannaCoinProFees.sol
+++ contracts/CannaCoinProFees.sol
@@ -4,28 +4,19 @@ pragma solidity ^0.8.23;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Cannacoin is ERC20, Ownable {
-    uint256 public constant INITIAL_SUPPLY = 100000000 * (10 ** 18);
+contract CannaCoinProFees is ERC20, Ownable {
     address public constant FEE_ADDRESS =
-        0x8114BeC86C8F56c1014f590E05cD7826054EcBdE;
-
-    // DEX Addresses
-    address public constant PANGOLIN_FACTORY =
-        0xefa94DE7a4656D787667C749f7E1223D71E9FD88;
+        0x3536b0152c91E60535508690a650C10bf09fe857;
     address public constant PANGOLIN_ROUTER =
         0xE54Ca86531e17Ef3616d22Ca28b0D458b6C89106;
     address public constant TRADER_JOE_ROUTER =
-        0x60aE616a2155Ee3d9A68541Ba4544862310933d4; // Replace with Trader Joe Router address
+        0x60aE616a2155Ee3d9A68541Ba4544862310933d4;
     address public constant YETISWAP_ROUTER =
-        0x262DcFB36766C88E6A7a2953c16F8defc40c378A; // Replace with YetiSwap Router address
+        0x262DcFB36766C88E6A7a2953c16F8defc40c378A;
 
     event FeePaid(address indexed from, address indexed to, uint256 feeAmount);
 
-    constructor(
-        address initialOwner
-    ) ERC20("Cannacoin PRO", "CPRO") Ownable(initialOwner) {
-        _mint(initialOwner, INITIAL_SUPPLY);
-    }
+    constructor() ERC20("Cannacoin PRO", "CPRO") Ownable(msg.sender) {}
 
     function transfer(
         address recipient,
@@ -33,18 +24,14 @@ contract Cannacoin is ERC20, Ownable {
     ) public override returns (bool) {
         uint256 fee = 0;
         if (isDexTransaction(msg.sender, recipient)) {
-            fee = calculateDexFee(amount); // 4.2% fee for DEX transactions
-            uint256 amountAfterFee = amount - fee;
-            _transfer(_msgSender(), recipient, amountAfterFee);
-        } else {
-            _transfer(_msgSender(), recipient, amount);
+            fee = calculateDexFee(amount);
         }
-
+        uint256 amountAfterFee = amount - fee;
+        _transfer(_msgSender(), recipient, amountAfterFee);
         if (fee > 0) {
             _transfer(_msgSender(), FEE_ADDRESS, fee);
             emit FeePaid(_msgSender(), FEE_ADDRESS, fee);
         }
-
         return true;
     }
 
@@ -53,8 +40,6 @@ contract Cannacoin is ERC20, Ownable {
         address recipient
     ) private pure returns (bool) {
         return
-            sender == PANGOLIN_FACTORY ||
-            recipient == PANGOLIN_FACTORY ||
             sender == PANGOLIN_ROUTER ||
             recipient == PANGOLIN_ROUTER ||
             sender == TRADER_JOE_ROUTER ||
@@ -64,6 +49,6 @@ contract Cannacoin is ERC20, Ownable {
     }
 
     function calculateDexFee(uint256 _value) private pure returns (uint256) {
-        return (_value * 42) / 1000; // 4.2% fee
+        return (_value * 42) / 1000;
     }
 }
