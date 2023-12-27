aebdd0fc6f2d93d1ab80c650eae273e234995f64
grasshaussoftware <deusopus@duck.com>
2023-12-26 16:04:41 -0800

    debugging

--- contracts/CannaCoinProMint.sol
+++ contracts/CannaCoinProMint.sol
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.23;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
 
 contract CannaCoinProMint is ERC20, Ownable {
     uint256 public constant INITIAL_SUPPLY = 100000000 * (10 ** 18);
 
     constructor() ERC20("Cannacoin PRO", "CPRO") Ownable(msg.sender) {
     _mint(msg.sender, INITIAL_SUPPLY);
-    }
-
+}
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,35 +1,32 @@
 require('dotenv').config();
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
-const { MNEMONIC } = process.env;
+const ganacheMnemonic = process.env.GANACHE_MNEMONIC;
+const avalancheMnemonic = process.env.AVALANCHE_MNEMONIC;
 
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 8545, // This is the default port for Ganache; change it if you use a different port
+      host: "10.0.0.10",
+      port: 8545, // Default port for Ganache
       network_id: "*", // Match any network
+      provider: () => new HDWalletProvider(ganacheMnemonic, "http://10.0.0.10:3000")
     },
     avalanche: {
-      provider: () => new HDWalletProvider({
-        mnemonic: {
-          phrase: MNEMONIC
-        },
-        providerOrUrl: "https://api.avax.network/ext/bc/C/rpc" // Avalanche C-Chain RPC URL
-      }),
+      provider: () => new HDWalletProvider(avalancheMnemonic, "https://api.avax.network/ext/bc/C/rpc"),
       network_id: 43114, // Avalanche C-Chain's network id
       gas: 3000000, // Standard gas limit
       gasPrice: 250000000000, // Average gas price in wei (250 Gwei)
       confirmations: 2,
       timeoutBlocks: 200,
-      skipDryRun: true // Set to false if you want to do a dry run
+      skipDryRun: true // Set to false for a dry run
     },
     // Additional network configurations go here
   },
 
   compilers: {
     solc: {
-      version: "0.8.20", // Use the same version as your contracts
+      version: "^0.8.23", //
       settings: {
         optimizer: {
           enabled: true,
